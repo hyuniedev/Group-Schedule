abe00f04a7bcb19b8cd206649e0505f981a91ae0
hyuniedev <huunien734@gmail.com>
2024-09-20 06:54:01 +0700

    + add color code

--- GroupSchedule/app/src/main/java/com/example/groupschedule/ui/theme/Color.kt
+++ GroupSchedule/app/src/main/java/com/example/groupschedule/ui/theme/Color.kt
@@ -9,3 +9,7 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Blue27 = Color(0xFF27A4F2)
+val Blue9F = Color(0xFF9FD7F9)
+val WhiteCF = Color(0xFFCFEBFC)
